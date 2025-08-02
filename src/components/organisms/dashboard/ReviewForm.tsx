19c8b5348010879ca33718f15b11e9fd0659e754
Ilman teguh prasetya <ilmanfrasetya@gmail.com>
2025-08-02 05:25:36 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/organisms/dashboard/ReviewForm.tsx
+++ src/components/organisms/dashboard/ReviewForm.tsx
@@ -14,6 +14,7 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
+import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Textarea } from "@/components/ui/textarea";
 import { useToast } from "@/hooks/use-toast";
